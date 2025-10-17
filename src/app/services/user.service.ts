ce6bdedaaadcdaa7b52d02e1393dad7751f798e2
Sebastian Gonzalez <sebastian.magm@gmail.com>
2025-10-17 01:48:09 -0300

    api agregada

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,7 +7,7 @@ import { User, ApiResponse } from '../interfaces/user.interface';
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:3000/api';
+  private apiUrl = 'https://backend-trzw.onrender.com/api';
 
   constructor(private http: HttpClient) { }
 
